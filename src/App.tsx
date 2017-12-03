8caf56a126c877e6e51164411c7db45fc261dbe8
Paolo Bueno <paolohaji@gmail.com>
2017-12-03 19:08:32 -0200

    Add navigation stub

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
-import Header from './Header';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Header />
-        
+        <Navigation />
       </div>
     );
   }
